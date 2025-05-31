3e1053d5c6278009726a16aa19c7c2d3638bc9fc
MADHURI <152051296+BoyaMadhuri@users.noreply.github.com>
2025-05-31 03:44:39 +0000

    2nd question completed

--- src/Module 3/Introduction to ReactJS/Ques_2.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_2.jsx
@@ -3,3 +3,15 @@
 // Use this component in App.js
 
 
+import React from 'react';
+
+function CurrentYear() {
+  const year = new Date().getFullYear(); // JavaScript expression to get current year
+  return (
+    <div>
+      <h1>The current year is {year}</h1>
+    </div>
+  );
+}
+
+export default CurrentYear;
