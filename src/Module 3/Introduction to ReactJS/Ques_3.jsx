309f8171970153189b90307306d2d623489d982c
MADHURI <152051296+BoyaMadhuri@users.noreply.github.com>
2025-05-31 03:47:10 +0000

    3rd question completed

--- src/Module 3/Introduction to ReactJS/Ques_3.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_3.jsx
@@ -1,3 +1,14 @@
 // Creating a Functional Component
 // Description: Change name of the current functional component into Greeting that returns a welcoming message.
 // Use this component in App.js
+import React from 'react';
+
+function Greeting() {
+  return (
+    <div>
+      <h1>Welcome to React Learning!</h1>
+    </div>
+  );
+}
+
+export default Greeting;
