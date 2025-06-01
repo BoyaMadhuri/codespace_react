01c3431776b8a285297f7cf0e63390c01af9d3b9
MADHURI <152051296+BoyaMadhuri@users.noreply.github.com>
2025-06-01 16:06:44 +0000

    todos

---
+++ src/Module 4/Hands-on Project - Build a Full-Stack Todo App/pages/TodosPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Todos from '../todos/Todos';
+
+function TodosPage() {
+  return (
+    <div>
+      <h2>Your To-Do List</h2>
+      <Todos />
+    </div>
+  );
+}
+
+export default TodosPage;
