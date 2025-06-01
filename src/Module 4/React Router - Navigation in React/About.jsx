66f48402c2ec8d1d23380f071e100aabd407f89f
MADHURI <152051296+BoyaMadhuri@users.noreply.github.com>
2025-06-01 12:45:24 +0000

    about

--- src/Module 4/React Router - Navigation in React/About.jsx
+++ src/Module 4/React Router - Navigation in React/About.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
     <div>
-      
+      <h1>About Us</h1>
+      <p>This page provides information about our application and team.</p>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
+
