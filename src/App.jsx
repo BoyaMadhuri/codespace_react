5d41ef647cee83d8fdcf07cd0547b7d1c154a8cf
MADHURI <152051296+BoyaMadhuri@users.noreply.github.com>
2025-06-01 15:16:43 +0000

    appj

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
-import Counter from './Module 4/Redux Toolkit - State Management in React/Counter_Redux/Counter';
+import Todos from './Module 4/Redux Toolkit - State Management in React/Todos_Redux/Todos';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div style={{ padding: '20px', textAlign: 'center' }}>
-        <h1>Redux Toolkit Counter</h1>
-        <Counter />
+      <div>
+        <Todos />
       </div>
     </Provider>
   );
