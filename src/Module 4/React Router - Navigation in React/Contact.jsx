eb55ecd50ad5d0482af9fe9ae879a8bbb38ae40b
MADHURI <152051296+BoyaMadhuri@users.noreply.github.com>
2025-06-01 12:46:32 +0000

    contact

--- src/Module 4/React Router - Navigation in React/Contact.jsx
+++ src/Module 4/React Router - Navigation in React/Contact.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
   return (
     <div>
-      
+      <h1>Contact Us</h1>
+      <p>Email: contact@example.com</p>
+      <p>Phone: +1 234 567 8901</p>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
+
